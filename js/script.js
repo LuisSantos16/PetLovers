d78b1798ee1e2bb0b83665d916ee3bfd0382f26d
LuisSantos16 <lmsr.1601@gmail.com>
2024-01-15 08:13:29 -0500

    header update 4.2

--- js/script.js
+++ js/script.js
@@ -14,19 +14,3 @@ function llamar() {
     }
 }
 
-//scroll
-window.addEventListener("scroll",Scroll);
-function Scroll(){
-    let MenuNavbar=document.getElementById("MenuNavbar");
-    let posy=window.pageYOffset;
-    if (posy>100){
-        MenuNavbar.style.position="fixed"
-        MenuNavbar.style.width="100%"
-        MenuNavbar.style.top="0"
-    }
-    else{
-        MenuNavbar.style.position="relative"
-    }
-}
-
-
